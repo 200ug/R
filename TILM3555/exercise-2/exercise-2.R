01f1d84c2c5bfeebedfaf0901cd529ff89857a21
17ms <79069176+17ms@users.noreply.github.com>
2024-02-27 01:26:34 +0200

    tilm3555 exercise 2 remake (latex)

--- TILM3555/exercise-2/exercise-2.R
+++ TILM3555/exercise-2/exercise-2.R
@@ -1,67 +1,58 @@
-# -- t. 1) --
+# t.1)
 
 x <- c(-3.4,-2.1,3.8,4.0,2.8,-4.3,-1.5,0.8,2.5,3.1,1.9,-6.0,-0.6,-16.6)
+boxplot(x)
+
+# Kvantiilien, otoskeskiarvon ja -keskihajonnan tarkistuslaskelmat
 quantile(x, probs=c(0.25, 0.5, 0.75), type=2)
 mean(x)
 sd(x)
 
-# -- t. 3) --
+# t.3)
 
-#################################################################
-terveys <- read.csv("C:\\Users\\einis\\Projects\\R\\TILM3555\\materials\\terveys.csv", sep=";")
-View(terveys)
-
-# Aineistoa paasee nyt tarkastelemaan RStudiossa editori-ikkunan
-# terveys-valilehdella. Riveina ovat tarkasteltavat havainnot
-# (ihmiset) ja sarakkeina muuttujien arvot.
-
-# Seuraava komento palauttaa aineiston koon (havaintojen lkm) ja
-# muuttujien lukumaaran:
-dim(terveys)
-
-# Muuttujien nimet saadaan seuraavasti:
-names(terveys)
-
-# Yksittaisen muuttujan arvoista koostuvaan vektoriin voidaan
-# viitata kayttamalla $-erotinta ja muuttujan nimea:
-terveys$ika
-
-# Piirretaan ika-muuttujan luokiteltu frekvenssijakauma eli
-# histogrammi jakamalla iat tasavalisesti 9 luokkaan:
-hist(terveys$ika, breaks=9)
-
-# table-funktiolla voidaan muodostaa frekvenssijakauma
-# muuttujalle. Sukupuolten frekvenssijakauma saadaan komennolla
-table(terveys$sukupuoli)
-
-# table-funktiolla voidaan muodostaa myos kaksiulotteinen
-# frekvenssijakauma eli ns. kontingenssitaulukko kahdelle
-# luokalliselle muuttujalle. Selvitetaan liikunnan harrastamista
-# sukupuolittain:
-taulukko <- table(terveys$sukupuoli, terveys$liikunta)
-taulukko
-
-# Edella R esittaa liikunnan 3 tasoa aakkosjarjestyksessa.
-# Kerrotaan luonnollinen jarjestys eksplisiittisesti ja yritetaan
-# uudestaan:
-terveys$liikunta <- factor(terveys$liikunta, levels=c("ei lainkaan","saannollisesti","paivittain"))
-taulukko <- table(terveys$sukupuoli, terveys$liikunta)
-taulukko
-
-# Piirretaan tasta summapylvaskuvio:
-barplot(taulukko, main="Liikunta sukupuolittain", col=c("blue", "red"), legend=c("miehet", "naiset")) 
-#################################################################
-
+terveys <- read.csv("/home/homer/Projects/uni-assignments/R/TILM3555/materials/terveys.csv", sep=";")
 terveys$bmi <- terveys$paino/(terveys$pituus/100)^2
-hist(terveys$bmi)
+
+# Painoindeksin histogrammi
+hist(terveys$bmi, main="Painoindeksin histogrammi")
+
+# Jaetaan aineisto miehiin ja naisiin
 terveys.m <- subset(terveys, sukupuoli=="Mies")
 terveys.n <- subset(terveys, sukupuoli=="Nainen")
 
-boxplot(terveys.n$bmi)
-qqnorm(terveys.n$bmi)
-qqline(terveys.n$bmi)
+# Tukeyn laatikkokuviot
+boxplot(terveys.m$bmi, main="Miehet")
+boxplot(terveys.n$bmi, main="Naiset")
 
-boxplot(terveys.m$bmi)
-qqnorm(terveys.m$bmi)
+# Normaalikvantiilikuviot
+qqnorm(terveys.m$bmi, main="Miehet")
 qqline(terveys.m$bmi)
 
+qqnorm(terveys.n$bmi, main="Naiset")
+qqline(terveys.n$bmi)
+
+# t.5)
+
+# Tehtävänannon vinouden määritelmään perustuva funktio
+vinous <- function(x) {
+  n <- length(x) # havaintojen lkm.
+  ka <- mean(x)  # otoskeskiarvo
+  (1/n)*sum((x-ka)^3) / ((1/n)*sum((x-ka)^2))^(3/2)
+}
+
+# Esimerkkituloksia (vinous<0: vasemmalle vino; vinous>0: oikealle vino)
+vinous(terveys$kolesteroli)
+vinous(terveys$pituus)
+vinous(terveys$paino)
+
+# Jakauman huipukkuuden laskeva funktio
+huipukkuus <- function(x) {
+  n <- length(x) # havaintojen lkm.
+  ka <- mean(x)  # otoskeskiarvo
+  (1/n)*sum((x-ka)^4) / ((1/n)*sum((x-ka)^2))^2-3
+}
+
+# Esimerkkituloksia (huipukkuus<0: litteä; huipukkuus>0: huipukas)
+huipukkuus(terveys$kolesteroli)
+huipukkuus(terveys$pituus)
+huipukkuus(terveys$paino)
