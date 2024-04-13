211fa9ffa629d598a0c9b46335b239b968520af0
17ms <79069176+17ms@users.noreply.github.com>
2024-04-13 22:10:46 +0300

    tilm3702 special 1/2

---
+++ TILM3702/special-1/special-1.R
@@ -0,0 +1,47 @@
+library(foreign)
+kotit1.dat <- read.spss("~/Projects/uni-assignments/R/TILM3702/materials/elinolo.sav", to.data.frame=TRUE)
+# attach(kotit1.dat)
+
+# Esim. oma opiskelijanumero
+set.seed(2102331)
+
+# 700 kokoinen otos
+oma.otos <- kotit1.dat[sample(nrow(kotit1.dat), 700),]
+
+# (Tehtävät tehdään tällä omalla otoksella, voit muotoilla harjoituksissa käytettyjä R-koodeja)
+
+# Raportti, jossa on mallin oletusten tarkastelu ja aineiston kuvailu ja keskeiset tilastollisen 
+# mallin tulokset: Mitkä selittävät muuttujat ovat mallissasi merkitseviä (ja onko yhdysvaikutus 
+# merkitsevä)  ja millaisia eroja on selittävän muuttujan luokkien välillä. 
+
+# Kuvailevat tunnusluvut
+with(oma.otos, tapply(pala, list(kumu, ahtas), mean))    # Keskiarvot
+with(oma.otos, tapply(pala, list(kumu, ahtas), median))  # Mediaanit
+with(oma.otos, tapply(pala, list(kumu, ahtas), sd))      # Keskihajonnat
+
+# Alla oleva komento riittää, koska kullekin selittävän muuttujen luokkien yhdistelmälle
+# on ainakin kaksi havaintoa.
+
+# Shapiro-Wilkin testit
+with(oma.otos, tapply(pala, list(kumu, ahtas), function(x) shapiro.test(x)$p.value))
+
+# Tukeyn laatikko-janakuvio, joka visualisoi tutkittavia jakaumia
+boxplot(pala~interaction(kumu, ahtas), data=oma.otos, main="Tutkittavat jakaumat")
+
+# Hajontojen yhtäsuuruustestaus Levenen testillä
+attach(oma.otos)
+library(car)
+leveneTest(pala~kumu*ahtas)
+
+# Itse kaksisuuntainen varianssianalyysi
+malli <- anova(lm(pala~kumu*ahtas))
+malli
+malli_lm2 <- aov(malli)
+
+# Tukeyn testi yhdysvaikutukselle (erot selittäjien eri luokkien välillä)
+TukeyHSD(malli_lm2)
+
+
+
+
+
